2df942576fe0ce433b09398d63c58a86dd54bc62
Aimar <aimargende@gmail.com>
2023-02-07 09:33:20 +0100

    Cliente

--- src/ClasesGestor/GestorBBDD.java
+++ src/ClasesGestor/GestorBBDD.java
@@ -1,5 +1,83 @@
 package ClasesGestor;
 
-public class GestorBBDD {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import Clases.Cliente;
+
+public class GestorBBDD extends Conector{
+
+	PreparedStatement pt;
+	String sentencia;
+	// Gestor clientes-------------------------------------------------------
+	public void registrarCliente(Cliente cliente) throws SQLException {
+		sentencia="INSERT INTO clientes VALUES (?,?,?,?,?)";
+		
+		pt=getCon().prepareStatement(sentencia);
+		
+		pt.setString(1,cliente.getDni());
+		pt.setString(2,cliente.getNombre());
+		pt.setString(3,cliente.getApellidos());
+		pt.setString(4,cliente.getDireccion());
+		pt.setString(5,cliente.getLocalidad());
+		
+		pt.execute();
+	}
+	
+	public Cliente getCliente(String dni) throws SQLException {
+		sentencia="SELECT * FROM clientes WHERE dni=?";
+		Cliente cliente = new Cliente();
+		
+		pt=getCon().prepareStatement(sentencia);
+		pt.setString(1, dni);
+		
+		ResultSet result=pt.executeQuery();
+		result.next();
+		
+		cliente.setDni(result.getString("dni"));
+		cliente.setNombre(result.getString("nombre"));
+		cliente.setApellidos(result.getString("apellidos"));
+		cliente.setDireccion(result.getString("direccion"));
+		cliente.setLocalidad(result.getString("localidad"));
+		
+		return cliente;
+	}
+	
+	public ArrayList<Cliente> getClientes() throws SQLException{
+		sentencia="SELECT * FROM clientes";
+		ArrayList<Cliente> clientes= new ArrayList<Cliente>();
+		
+		pt=getCon().prepareStatement(sentencia);
+		ResultSet result=pt.executeQuery();
+		
+		while(result.next()) {
+			Cliente cliente= new Cliente();
+			
+			cliente.setDni(result.getString("dni"));
+			cliente.setNombre(result.getString("nombre"));
+			cliente.setApellidos(result.getString("apellidos"));
+			cliente.setDireccion(result.getString("direccion"));
+			cliente.setLocalidad(result.getString("localidad"));
+			
+			clientes.add(cliente);
+		}
+		
+		return clientes;
+	}
+	
+	
+	
+// Gestor reservas-------------------------------------------------------
+	public void realizarReserva(String dni) {
+		Cliente cliente=new Cliente();
+		try {
+			cliente=getCliente(dni);
+		}
+		catch(Exception e) {
+			System.out.println("No existe el cliente");
+		}
+	}
+// Gestor de hoteles-----------------------------------------------------
 }
