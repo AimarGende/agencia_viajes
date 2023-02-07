22ecf54051230058bcbca09f9793eb1a0409b2db
Aimar <aimargende@gmail.com>
2023-02-07 09:44:08 +0100

    estructura

--- src/Gestores/GestorClientes.java
+++ src/Gestores/GestorClientes.java
@@ -1,10 +1,47 @@
 package Gestores;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
 import Clases.Cliente;
 import Complementarios.*;
 import ClasesGestor.*;
 
 public class GestorClientes {
-	GestorBBDD gest= new GestorBBDD();
-	Cliente cliente= new Cliente();
+	public static void run(Scanner sc) throws ClassNotFoundException, SQLException {
+		
+		GestorBBDD gest= new GestorBBDD();
+		Cliente cliente= new Cliente();
+		int opcion=0;
+		
+		do {
+			opcion=Integer.parseInt(sc.nextLine());
+			
+			switch (opcion) {
+			case Menus.REGISTRAR_CLIENTES: 
+				gest.conectar();
+				
+				
+				gest.cerrar();
+				break;
+			case Menus.BUSCAR_CLIENTE:
+				gest.conectar();
+				
+				
+				gest.cerrar();
+				break;
+			case Menus.MOSTRAR_CLIENTES:
+				gest.conectar();
+				
+				
+				gest.cerrar();
+				break;
+			default:
+				System.out.println("La opcion no es valida");
+			}
+			
+		}while(opcion!=Menus.SALIR);
+		System.out.println("Saliendo del menu");
+	}
+
 }
