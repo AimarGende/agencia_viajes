2c0264e995912169df6e7aef7715c2e04d9630bb
Aimar <aimargende@gmail.com>
2023-02-07 09:45:16 +0100

    Estructura

--- src/Gestores/GestorHotel.java
+++ src/Gestores/GestorHotel.java
@@ -1,5 +1,38 @@
 package Gestores;
 
-public class GestorHoteles_Habitaciones {
+import java.sql.SQLException;
+import java.util.Scanner;
 
+import Clases.Habitacion;
+import Clases.Hotel;
+import ClasesGestor.GestorBBDD;
+import Complementarios.Menus;
+
+public class GestorHotel {
+public static void run(Scanner sc) throws ClassNotFoundException, SQLException {
+		
+		GestorBBDD gest= new GestorBBDD();
+		Hotel hotel=new Hotel();
+		Habitacion habitacion = new Habitacion();
+		
+		int opcion=0;
+		
+		do {
+			opcion=Integer.parseInt(sc.nextLine());
+			
+			switch (opcion) {
+			case Menus.ALTA_HOTEL: 
+				gest.conectar();
+				
+				
+				gest.cerrar();
+				break;
+
+			default:
+				System.out.println("La opcion no es valida");
+			}
+			
+		}while(opcion!=Menus.SALIR);
+		System.out.println("Saliendo del menu");
+	}
 }
