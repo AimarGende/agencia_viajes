db4700e8e0238e3e085df88d7ac9abca796261cc
Aimar <aimargende@gmail.com>
2023-02-08 14:24:23 +0100

    Revisar

--- src/Gestores/GestorReservas.java
+++ src/Gestores/GestorReservas.java
@@ -1,18 +1,21 @@
 package Gestores;
 
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.Scanner;
 
 import Clases.Reserva;
 import ClasesGestor.GestorBBDD;
+import Complementarios.FormularioDeDatos;
 import Complementarios.Menus;
 
 public class GestorReservas {
-public static void run(Scanner sc) throws ClassNotFoundException, SQLException {
+public static void run(Scanner sc) throws ClassNotFoundException, SQLException, ParseException {
 		
 		GestorBBDD gest= new GestorBBDD();
 		Reserva reserva= new Reserva();
 		int opcion=0;
+		
 		
 		do {
 			Menus.menuReserva();
@@ -21,14 +24,14 @@ public static void run(Scanner sc) throws ClassNotFoundException, SQLException {
 			switch (opcion) {
 			case Menus.REALIZAR_RESERVA: 
 				gest.conectar();
-				//Pedir dni cliente
-				
+				String dni=FormularioDeDatos.pedirDniCliente(sc);
+				gest.realizarReserva(dni, sc);
 				gest.cerrar();
 				break;
 			case Menus.ANULAR_RESERVA:
 				gest.conectar();
-				
-				
+				int id=FormularioDeDatos.pedirIdReserva(sc);
+				gest.anularReserva(id);
 				gest.cerrar();
 				break;
 			default:
