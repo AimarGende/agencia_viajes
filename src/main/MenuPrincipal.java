980655f3b1d3ac246505575fcee9bfee4432da24
Aimar <aimargende@gmail.com>
2023-02-08 11:51:58 +0100

    acabado

--- src/main/MenuPrincipal.java
+++ src/main/MenuPrincipal.java
@@ -1,5 +1,34 @@
 package main;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+import Complementarios.Menus;
+import Gestores.*;
+
 public class MenuPrincipal {
+	
+	public static void run() throws ClassNotFoundException, SQLException {
+		Scanner sc=new Scanner(System.in);
+		int opcion=0;
+		
+		do {
+			Menus.menuPrincipal();
+			opcion=Integer.parseInt(sc.nextLine());
+			
+			switch(opcion) {
+				case Menus.GESTION_CLIENTES:
+					GestorClientes.run(sc);
+					break;
+				case Menus.GESTION_RESERVAS:
+					GestorReservas.run(sc);
+					break;
+				case Menus.GESTION_HOTELES:
+					GestorHotel.run(sc);
+					break;
+			}
+		}while(opcion!=Menus.SALIR);
+		
+		System.out.println("Gracias por usar nuestra aplicacion!");
+	}
 
 }
--- src/main/main.java
+++ src/main/main.java
@@ -1,5 +1,11 @@
 package main;
 
+import java.sql.SQLException;
+
 public class main {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		MenuPrincipal.run();
+	}
 
 }
